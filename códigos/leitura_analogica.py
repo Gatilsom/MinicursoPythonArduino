966135feb5f3461c97cb3f3b9cd900db7f191c6c
Gatilsom <107775815+Gatilsom@users.noreply.github.com>
2022-07-22 10:11:55 -0300

    Add files via upload

---
+++ códigos/leitura_analogica.py
@@ -0,0 +1,30 @@
+
+from time import sleep
+from pyfirmata2 import *
+
+def myCallback(data):
+    print(data)
+
+placa = Arduino('COM3')
+# -> iniciar a leitura analógica
+# -> deve-se informar o período de amostragem em ms
+F = 100  # frequência de amostragem em Hz
+placa.samplingOn(1000/F)
+
+# -> definição da porta
+pin = placa.get_pin('a:0:i')  # analógico: pino A0: entrada
+
+# -> é necessário registrar uma função de callback, ou seja,
+# -> uma função que será chamada automaticamente a cada intervalo de tempo
+# -> definido anteriormente pelo método samplingOn
+pin.register_callback(myCallback)
+pin.enable_reporting()
+
+# -> loop para execução do programa
+while True:
+    sleep(0.1)
+
+
+
+
+
