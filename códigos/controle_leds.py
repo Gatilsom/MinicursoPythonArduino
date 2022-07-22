ac6cf8324e9e77d44192a8d87833bfb6309f1d84
Gatilsom <107775815+Gatilsom@users.noreply.github.com>
2022-07-22 08:48:02 -0300

    Add files via upload

---
+++ códigos/controle_leds.py
@@ -0,0 +1,23 @@
+
+from time import sleep
+from pyfirmata2 import *
+
+placa = Arduino('COM3')
+
+pin_led_r = placa.get_pin('d:8:o')
+pin_led_y = placa.get_pin('d:9:o')
+pin_led_g = placa.get_pin('d:10:o')
+
+while True:
+    pin_led_r.write(True)
+    pin_led_y.write(True)
+    pin_led_g.write(True)
+    sleep(1)  # atraso de 1 segundo
+    pin_led_r.write(False)
+    pin_led_y.write(False)
+    pin_led_g.write(False)
+    sleep(1)  # atraso de 1 segundo
+
+
+
+
